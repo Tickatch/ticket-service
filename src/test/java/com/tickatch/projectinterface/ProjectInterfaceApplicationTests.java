5cd710d64383e4a5437cece395d330f48a137718
sojsnake <93419110+sojsnake@users.noreply.github.com>
2025-12-02 17:03:10 +0900

    Initial commit

---
+++ src/test/java/com/tickatch/projectinterface/ProjectInterfaceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tickatch.projectinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectInterfaceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
